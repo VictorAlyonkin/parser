55b5dd8ea5dd2f6c857fb54a47cdba0de3c67246
Victor Alyonkin <viktoalenkin@ya.ru>
2022-10-19 09:23:46 +0300

    add xml parser

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,22 +5,45 @@ import com.opencsv.CSVReader;
 import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException {
+        List<Employee> employees;
+        String fileNameCsv = "data.csv";
+        String fileNameXml = "data.xml";
+        String fileNameJsonCsv = "data.json";
+        String fileNameJsonXml = "data2.json";
+        String json;
+        //Первое задание
         String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
-        String fileNameInput = "data.csv";
-        String fileNameOuput = "data.json";
-        List<Employee> employees = parseCSV(columnMapping, fileNameInput);
-        String json = listToJson(employees);
-        writeString(json, fileNameOuput);
+
+        employees = parseCSV(columnMapping, fileNameCsv);
+        json = listToJson(employees);
+        writeString(json, fileNameJsonCsv);
+        System.out.println(json);
+        employees.clear();
+        //Второе задание
+        System.out.println("---------------------");
+
+        employees = parseXML("data.xml");
+        employees.forEach(System.out::println);
+        json = listToJson(employees);
+        writeString(json, fileNameJsonXml);
         System.out.println(json);
     }
 
@@ -54,5 +77,47 @@ public class Main {
                 System.out.println(e.getMessage());
             }
         }
+    }
+
+    private static List<Employee> parseXML(String fileName) throws ParserConfigurationException, IOException, SAXException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse(new File(fileName));
+
+        Node root = doc.getDocumentElement();
+        System.out.println(root.getNodeName());
+        return read(root);
+    }
+
+    private static List<Employee> read(Node node) {
+        List<Employee> employees = new ArrayList<>();
+        List<String> attributesText = new ArrayList<>();
+        NodeList nodeList = node.getChildNodes();
+
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node node_ = nodeList.item(i);
+            if (Node.ELEMENT_NODE == node_.getNodeType() &&
+                    "employee".equals(node_.getNodeName())) {
+                NodeList nodeEmployees = node_.getChildNodes();
+                for (int a = 0; a < nodeEmployees.getLength(); a++) {
+                    Node nodeEmployee = nodeEmployees.item(a);
+                    if (Node.ELEMENT_NODE == nodeEmployee.getNodeType()) {
+                        attributesText.add(nodeEmployee.getTextContent());
+                    }
+                }
+                employees.add(
+                        new Employee(
+                                Long.parseLong(attributesText.get(0)),
+                                attributesText.get(1),
+                                attributesText.get(2),
+                                attributesText.get(3),
+                                Integer.parseInt(attributesText.get(4))
+                        )
+                );
+                attributesText.clear();
+            }
+            read(node_);
+        }
+        return employees;
     }
 }
